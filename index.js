a641f6ba3316ac81aabf1ddf9e36f4af3a0a7cb5
zer0fire <1076671121@qq.com>
2021-11-23 01:16:28 +0800

    chore: 初始化和 TODO

---
+++ index.js
@@ -0,0 +1,15 @@
+const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const progress = require('progress')
+const { promisify } = require('util')
+const fs = require('fs')
+const childProcess = require('child_process')
+const process = require('process')
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const readStat = promisify(fs.stat)
+
+// program.
+

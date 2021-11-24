b994e7f350b59abd3ddcc5e7b011c17270f98514
wangmeng08 <wangmeng08@huohua.cn>
2021-11-24 10:27:04 +0800

    chore: 初始化

---
+++ bin/run.js
@@ -0,0 +1,6 @@
+try {
+    require('../index')
+} catch (e) {
+    // TODO: 报错信息
+    console.error('run error')
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+const promisify = require('util').promisify
+const program = require('commander')
